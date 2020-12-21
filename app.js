9b9967d456b2889ddd5f4fc80efceca17b864983
yibizzi <ibizzi.younes@ine.inpt.ma>
2020-12-21 16:19:28 +0100

    added implementation and data model

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-const firebase=require("firebase");
-const fs=require('fs');
+const admin = require('./node_modules/firebase-admin');
+//must add service-key.json that contains firebase credentials
+const serviceAccount = require("./service-key.json");
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBGtJe_nloVjWrJtmnbOmuS4QM5be_qE40",
-    authDomain: "movies-cb314.firebaseapp.com",
-    projectId: "movies-cb314",
-    storageBucket: "movies-cb314.appspot.com",
-    messagingSenderId: "201149802523",
-    appId: "1:201149802523:web:dc46c8b30d2f7ee7bdf0be"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+const data = require("./data.json");
 
-  const db = firebase.firestore();
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "ADD_DATABASE_URL: https://app_name.firebaseio.com"
+});
 
-//redaing json 
-let rawdata=fs.readFileSync('test.json');
-let data=JSON.parse(rawdata);
+data && Object.keys(data).forEach(key => {
+    const nestedContent = data[key];
 
-const coll =data.data;
-
-coll.forEach(function(obj) {
-    db.collection("coll").add({
-        id: obj.id,
-        name: obj.name,
-        description: obj.description
-    }).then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
+    if (typeof nestedContent === "object") {
+        Object.keys(nestedContent).forEach(docTitle => {
+            admin.firestore()
+                .collection(key)
+                .doc(docTitle)
+                .set(nestedContent[docTitle])
+                .then((res) => {
+                    console.log("Document successfully written!");
+                })
+                .catch((error) => {
+                    console.error("Error writing document: ", error);
+                });
+        });
+    }
 });
